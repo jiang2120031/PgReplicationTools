9d01c53e2c8846dad42c9e0f83a6c75d22338c51
fei\Yongjie.Wang <Yongjie.Wang@feisystems.com>
2018-03-29 14:20:48 +0800

    create project

---
+++ PgReplication/PgReplication/command/SqlHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using System.Configuration;
+namespace PgReplication.command
+{
+    public class SqlHelper
+    {
+        private static readonly string sqlConnection = "";
+
+    }
+}
